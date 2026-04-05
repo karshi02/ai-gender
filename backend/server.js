94caba6eca96937cf24e8d3624ff2b1d200e8109
karshi02 <karshi0234@gmail.com>
2026-04-05 13:22:38 +0700

    fix backgrud all photo

--- backend/server.js
+++ backend/server.js
@@ -235,6 +235,8 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '../frontend')));
+// app.use(express.static(path.join(__dirname, '../frontend'))); // ✅ serve static จาก /themes แทน
+
 
 const uploadDir = path.join(__dirname, '../uploads');
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
